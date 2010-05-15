4e0b01126ac5b9f259f358c062f24aa1595a0dd0
Bacon Dave <dbacon@Bacon-Daves-MacBook-Pro.local>
2010-05-15 21:30:18 +0900

    Release 1.0.8 (remove all -snapshot labels).

--- timeslice-host/re-test.sh
+++ timeslice-host/re-test.sh
@@ -2,15 +2,17 @@
 set -e
 set -x
 
+version="1.0.8"
+
 rm -rf "$HOME"/.timeslice*
 rm -rf tmp-deploy
 mkdir tmp-deploy
 
 cd tmp-deploy
 
-unzip ../target/timeslice-host-1.0.8-SNAPSHOT-dist.zip
+unzip "../target/timeslice-host-${version}-dist.zip"
 
-cd timeslice-host-1.0.8-SNAPSHOT
+cd "timeslice-host-${version}"
 
 bin/runme.sh -p 1080
 
