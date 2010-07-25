1e126d3b20209e21c6f6e264d325183b44c8781c
Bacon Dave <dbacon@Bacon-Daves-MacBook-Pro.local>
2010-07-25 19:06:41 +0900

    javadoc bacond.lib.util.Check null check methods.

--- timeslice-lib/src/main/java/bacond/lib/util/Check.java
+++ timeslice-lib/src/main/java/bacond/lib/util/Check.java
@@ -2,26 +2,47 @@ package bacond.lib.util;
 
 public class Check
 {
-	public static void notNull(Object o, String s)
-	{
-		if (null == o)
-		{
-			throw new RuntimeException("Object must not be null: " + s);
-		}
-	}
+    /**
+     * @deprecated - use {@link disallowNull} (if you must, see API docs).
+     * @param o
+     * @param s
+     */
+    @Deprecated
+    public static void notNull(Object o, String s)
+    {
+        if (null == o)
+        {
+            throw new RuntimeException("Object must not be null: " + s);
+        }
+    }
 
-	public static <T> T disallowNull(T t)
-	{
-		return disallowNull(t, null);
-	}
+    /**
+     * Use only for defensive checks in external-facing API (library code).
+     *
+     * @param <T>
+     * @param t
+     * @return
+     */
+    public static <T> T disallowNull(T t)
+    {
+        return disallowNull(t, null);
+    }
 
-	public static <T> T disallowNull(T t, String name)
-	{
-		if (null == t)
-		{
-			throw new RuntimeException("Null not allowed" + ((null == name) ? "" : (" for '" + name + "'")) + ".");
-		}
+    /**
+     * Use only for defensive checks in external-facing API (library code).
+     *
+     * @param <T>
+     * @param t
+     * @param name
+     * @return
+     */
+    public static <T> T disallowNull(T t, String name)
+    {
+        if (null == t)
+        {
+            throw new RuntimeException("Null not allowed" + ((null == name) ? "" : (" for '" + name + "'")) + ".");
+        }
 
-		return t;
-	}
+        return t;
+    }
 }
