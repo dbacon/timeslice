18ab5bf9ffc42737716ee0df0ab59e73fa659b28
Dave Bacon <drb.code@mrbacon.com>
2010-07-31 23:06:04 +0900

    Factor out session-tracking from gwt-service impl to separate class.

--- timeslice-web/src/main/java/bacond/timeslice/web/gwt/server/rpc/TimesliceSvc.java
+++ timeslice-web/src/main/java/bacond/timeslice/web/gwt/server/rpc/TimesliceSvc.java
@@ -4,9 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 import org.joda.time.DateTimeZone;
@@ -20,7 +18,6 @@ import bacond.timeslice.web.gwt.client.server.ITimesliceSvc;
 import bacond.timeslice.web.gwt.client.server.ProcType;
 import bacond.timeslice.web.gwt.client.server.SortDir;
 import bacond.timeslice.web.gwt.server.beantx.ServerToClient;
-import bacond.timeslicer.app.core.AclFile;
 import bacond.timeslicer.app.core.Aggregate;
 import bacond.timeslicer.app.core.Sum;
 import bacond.timeslicer.timeslice.TimesliceApp;
@@ -31,21 +28,9 @@ public class TimesliceSvc extends RemoteServiceServlet implements ITimesliceSvc
 {
     private static final long serialVersionUID = 1L;
 
-    private Map<String, SessionData> validSessions = new LinkedHashMap<String, SessionData>();
-
     private Sum summer = new Sum();
     private Aggregate aggregator = new Aggregate();
-
-    protected TimesliceApp getTimesliceApp()
-    {
-        return TimesliceStartupServletContextListener.getTimesliceApp(getServletContext());
-    }
-
-    @Override
-    public void logout(String authenticationToken)
-    {
-        validSessions.remove(authenticationToken);
-    }
+    private SessionTracker sessionTracker = new SessionTracker();
 
     @Override
     public String serverInfo()
@@ -53,49 +38,28 @@ public class TimesliceSvc extends RemoteServiceServlet implements ITimesliceSvc
         return "1.0.10-SNAPSHOT"; // TODO: get from resources/build
     }
 
+    protected TimesliceApp getTimesliceApp()
+    {
+        return TimesliceStartupServletContextListener.getTimesliceApp(getServletContext());
+    }
+    
+
     @Override
     public String authenticate(String username, String password)
     {
-        System.out.println("authenticate(" + username + ").");
-
-        // TODO: implement hashing the pw, lookup in db, applying authorization.
-        // for now, just make sure it matches what's in their acl.
-        String aclFileName = getTimesliceApp().getAclFileName();
-        if (null == aclFileName) return null;
-
-        String realPw = new AclFile(aclFileName).lookupPassword(username);
-        if (null == realPw) return null;
-
-        if (!realPw.equals(password)) return null;
-
-        SessionData sd = new SessionData(username);
-        validSessions.put(sd.getUuid(), sd);
-
-        return sd.getUuid();
+        return sessionTracker.authenticate(getTimesliceApp(), username, password);
     }
 
-    protected SessionData checkToken(String authenticationToken)
+    @Override
+    public void logout(String authToken) throws NotAuthenticException
     {
-        if (!validSessions.containsKey(authenticationToken))
-        {
-            throw new NotAuthenticException("Invalid token.");
-        }
-
-        SessionData sessionData = validSessions.get(authenticationToken);
-        if (sessionData.getExpiresAt().isBeforeNow())
-        {
-            SessionData expiredSession = validSessions.remove(authenticationToken);
-            throw new NotAuthenticException("Expired token: " + expiredSession.expiresAt.toString());
-        }
-
-        // great, have a nice time.
-        return sessionData;
+        sessionTracker.logout(authToken);
     }
 
     @Override
     public List<StartTag> refreshItems(String authToken, int maxSize, SortDir sortDir, ProcType procType, String startingInstant, String endingInstant)
     {
-        SessionData sd = checkToken(authToken);
+        SessionData sd = sessionTracker.checkToken(authToken);
 
         return new ArrayList<StartTag>(Transform.tr(getTimesliceApp().queryForTags(
                 sd.getUser(),
@@ -181,7 +145,7 @@ public class TimesliceSvc extends RemoteServiceServlet implements ITimesliceSvc
     @Override
     public List<TaskTotal> refreshTotals(String authToken, int maxSize, SortDir sortDir, ProcType procType, String startingInstant, String endingInstant, List<String> allowWords, List<String> ignoreWords)
     {
-        SessionData sd = checkToken(authToken);
+        SessionData sd = sessionTracker.checkToken(authToken);
 
         List<bacond.timeslicer.app.core.StartTag> tags = getTimesliceApp().queryForTags(
                 sd.getUser(),
@@ -205,7 +169,7 @@ public class TimesliceSvc extends RemoteServiceServlet implements ITimesliceSvc
     @Override
     public String persistTotals(String authToken, String persistAsName, int maxSize, SortDir sortDir, ProcType procType, String startingInstant, String endingInstant, List<String> allowWords, List<String> ignoreWords)
     {
-        SessionData sd = checkToken(authToken);
+        SessionData sd = sessionTracker.checkToken(authToken);
 
         List<bacond.timeslicer.app.core.StartTag> tags = getTimesliceApp().queryForTags(
                 sd.getUser(),
@@ -257,7 +221,7 @@ public class TimesliceSvc extends RemoteServiceServlet implements ITimesliceSvc
     @Override
     public void addItem(String authToken, String instantString, String taskDescription)
     {
-        SessionData sd = checkToken(authToken);
+        SessionData sd = sessionTracker.checkToken(authToken);
         throwIfNoAvailableStore();
 
         getTimesliceApp().getFrontStore().add(new bacond.timeslicer.app.core.StartTag(sd.getUser(), instantString, taskDescription, null));
@@ -266,7 +230,7 @@ public class TimesliceSvc extends RemoteServiceServlet implements ITimesliceSvc
     @Override
     public void addItems(String authToken, List<StartTag> items)
     {
-        SessionData sd = checkToken(authToken);
+        SessionData sd = sessionTracker.checkToken(authToken);
         throwIfNoAvailableStore();
 
         for (StartTag item: items)
@@ -286,7 +250,7 @@ public class TimesliceSvc extends RemoteServiceServlet implements ITimesliceSvc
     @Override
     public void update(String authToken, StartTag editedStartTag)
     {
-        SessionData sd = checkToken(authToken);
+        SessionData sd = sessionTracker.checkToken(authToken);
         throwIfNoAvailableStore();
 
         bacond.timeslicer.app.core.StartTag edited = new bacond.timeslicer.app.core.StartTag(sd.getUser(), editedStartTag.getInstantString(), editedStartTag.getDescription(), null);
