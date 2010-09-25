33db145d5466b25182771a06a04f7831a1c7c6d8
Bacon Dave <dbacon@Bacon-Daves-MacBook-Pro.local>
2010-09-25 23:06:29 +0900

    Add test for version 1.0.7 data format loader.

---
+++ timeslice-web/src/test/java/com/enokinomi/timeslice/web/gwt/client/task/ui_compat/Ts107ReaderTest.java
@@ -0,0 +1,24 @@
+package com.enokinomi.timeslice.web.gwt.client.task.ui_compat;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.enokinomi.timeslice.web.gwt.client.task.core.StartTag;
+
+
+public class Ts107ReaderTest
+{
+
+    @Test
+    public void test1()
+    {
+        Ts107Reader r = new Ts107Reader("[monkey#2010-09-11T12:03:03#monkey]");
+        List<StartTag> parsed = r.parseItems();
+
+        assertNotNull(parsed);
+        assertEquals(1, parsed.size());
+    }
+}
