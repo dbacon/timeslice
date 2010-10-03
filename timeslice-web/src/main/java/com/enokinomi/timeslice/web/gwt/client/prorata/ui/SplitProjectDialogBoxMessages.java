4db79319b231424256f1389312df714691b636f6
Bacon Dave <dbacon@Bacon-Daves-MacBook-Pro.local>
2010-10-03 17:40:27 +0900

    Add Japanese translation for split-project dialog box.

---
+++ timeslice-web/src/main/java/com/enokinomi/timeslice/web/gwt/client/prorata/ui/SplitProjectDialogBoxMessages.java
@@ -0,0 +1,9 @@
+package com.enokinomi.timeslice.web.gwt.client.prorata.ui;
+
+import com.google.gwt.i18n.client.Messages;
+
+public interface SplitProjectDialogBoxMessages extends Messages
+{
+    @DefaultMessage("Split project ''{0}''")
+    String splitProjectTitle(String projectName);
+}
