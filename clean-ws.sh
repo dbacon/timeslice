b2284aa66959213c132258a11557078336334d9f
Bacon Dave <dbacon@Bacon-Daves-MacBook-Pro.local>
2010-10-16 13:42:48 +0900

    Add *.gwt.xml for cleaning whitespace;

--- clean-ws.sh
+++ clean-ws.sh
@@ -3,4 +3,5 @@
 find . -name \*.java   -exec ./strip-whitespace.sh {} \;
 find . -name \*.css    -exec ./strip-whitespace.sh {} \;
 find . -name pom.xml   -exec ./strip-whitespace.sh {} \;
+find . -name \*.gwt.xml   -exec ./strip-whitespace.sh {} \;
 
