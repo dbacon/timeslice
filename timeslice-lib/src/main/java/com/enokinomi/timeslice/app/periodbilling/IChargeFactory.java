3f24e19238b802ad4cf6322004ac15bad7512ae5
Bacon Dave <dbacon@Bacon-Daves-MacBook-Pro.local>
2010-10-17 16:04:29 +0900

    Remove unused packages: periodbilling, rolodex, and todo;

--- timeslice-lib/src/main/java/com/enokinomi/timeslice/app/periodbilling/IChargeFactory.java
+++
@@ -1,9 +0,0 @@
-package com.enokinomi.timeslice.app.periodbilling;
-
-import java.util.List;
-
-
-public interface IChargeFactory
-{
-    List<Charge> createCharges(long millis);
-}
--- timeslice-lib/src/main/java/com/enokinomi/timeslice/app/periodbilling/IChargeFactoryResolver.java
+++
@@ -1,7 +0,0 @@
-package com.enokinomi.timeslice.app.periodbilling;
-
-
-public interface IChargeFactoryResolver
-{
-    public IChargeFactory resolve(String name);
-}
