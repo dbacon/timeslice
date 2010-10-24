d9c2f1b84303e97ba71f575aeca6e8547e4f3b5b
Bacon Dave <dbacon@Bacon-Daves-MacBook-Pro.local>
2010-10-24 20:50:50 +0900

    Turn off downgrade/testing app-jobs.

--- timeslice-lib/src/main/java/com/enokinomi/timeslice/lib/appjob_stockjobs/StockJobsModule.java
+++ timeslice-lib/src/main/java/com/enokinomi/timeslice/lib/appjob_stockjobs/StockJobsModule.java
@@ -16,11 +16,11 @@ public class StockJobsModule extends AbstractModule
 //        appJobSetBinder.addBinding().toInstance(new TestJob1("test-job-2-succeeds"));
 //        appJobSetBinder.addBinding().toInstance(new FailingTestJob("test-job-2-fails"));
 
-        appJobSetBinder.addBinding().to(SchemaOpAppJob.class);
-
-        appJobSetBinder.addBinding().to(DowngradeSchema1To0AppJob.class);
-        appJobSetBinder.addBinding().to(Version3To2DowngradeSchemaAppJob.class);
-        appJobSetBinder.addBinding().to(Version4To3DowngradeSchemaAppJob.class);
+//        appJobSetBinder.addBinding().to(SchemaOpAppJob.class);
+//
+//        appJobSetBinder.addBinding().to(DowngradeSchema1To0AppJob.class);
+//        appJobSetBinder.addBinding().to(Version3To2DowngradeSchemaAppJob.class);
+//        appJobSetBinder.addBinding().to(Version4To3DowngradeSchemaAppJob.class);
 
         appJobSetBinder.addBinding().to(ListTablesAppJob.class);
         appJobSetBinder.addBinding().to(DetectSchemaVersionAppJob.class);
