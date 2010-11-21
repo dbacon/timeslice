fbf1fa51ec63436504e02668eedf4cdc57b2f58d
Bacon Dave <dbacon@Bacon-Daves-MacBook-Pro.local>
2010-11-21 23:26:25 +0900

    Add missing binding for new ITimesliceSvc split (in lib).

--- timeslice-lib/src/main/java/com/enokinomi/timeslice/lib/task/impl/TaskModule.java
+++ timeslice-lib/src/main/java/com/enokinomi/timeslice/lib/task/impl/TaskModule.java
@@ -2,6 +2,7 @@ package com.enokinomi.timeslice.lib.task.impl;
 
 import com.enokinomi.timeslice.lib.task.api.ISafeDirProvider;
 import com.enokinomi.timeslice.lib.task.api.ITimesliceStore;
+import com.enokinomi.timeslice.lib.task.api.ITimesliceSvc;
 import com.enokinomi.timeslice.lib.task.api.ITimesliceWorks;
 import com.google.inject.AbstractModule;
 
@@ -11,6 +12,7 @@ public class TaskModule extends AbstractModule
     @Override
     protected void configure()
     {
+        bind(ITimesliceSvc.class).to(TimesliceSvc.class);
         bind(ITimesliceStore.class).to(HsqldbTimesliceStore.class);
         bind(ITimesliceWorks.class).to(TimesliceWorks.class);
         bind(ISafeDirProvider.class).to(SafeDirProvider.class);
